df50e41ac3b0c0535bc012b1aa3f9412ab27be72
Spitamen2018 <sukhrobazimov1996@gmail.com>
2023-11-20 16:34:13 +0500

    added some rules

---
+++ app/Rules/NestedCategoryRule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class NestedCategoryRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        //
+    }
+}
