93a13253a7280f3810a85c95af94193a748fe1a7
Spitamen2018 <sukhrobazimov1996@gmail.com>
2023-11-20 16:24:30 +0500

    added consumer and publisher rmq

---
+++ app/Console/Commands/RmqConsumeer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RmqConsumeer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rmq:consumeer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
